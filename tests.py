2ff310b23dd45cc46d440e16429fef5e5bfd7ff5
Will <75584941+englsh@users.noreply.github.com>
2026-09-22 23:30:10 +0100

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,25 @@
+'''
+NEED TO ADD:
+    REQUESTS FRAME / WHATEVER THE FUCK ITS CALLED (I HAVENT READ THE DOCS YET LOL)
+     - WHEREVER THIS LEADS ME
+'''
+
+from SettingsFrame import SettingsData
+import socket
+
+host = "example.com"
+
+connection = socket.create_connection((host, 80))
+
+connectionPreface = b"PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
+settingsFrame = SettingsData({
+    "SETTINGS_MAX_CONCURRENT_STREAMS": bytes.fromhex("00000064"),
+    "SETTINGS_INITIAL_WINDOW_SIZE": bytes.fromhex("0000ffff")
+})
+
+data = connectionPreface + settingsFrame.to_bytes()
+
+print(data)
+
+connection.send(data)
+print(connection.recv(1024))
